260b65a83831c3583c59fe9049c90f942f361553
Luthfi Saputra <luthfsptr@gmail.com>
2026-03-16 20:57:52 +0700

    fix: penambahan method poin untuk menjalankan program di Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
-    Kopi kopi1 = new Kopi("Kopi Susu", 25000);
-    Kopi kopi2 = new Kopi("Espresso", 30000);
+    public static void main(String[] args) {
+        Kopi kopi1 = new Kopi("Kopi Susu", 25000);
+        Kopi kopi2 = new Kopi("Espresso", 30000);
 
-    Pelanggan pelanggan = new Pelanggan("luthfi", 100000);
+        Pelanggan pelanggan = new Pelanggan("luthfi", 100000);
+    }
+
 }
