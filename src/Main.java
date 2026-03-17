d47bd8b8bb09b76177936912f3423035ff6d4638
Luthfi Saputra <luthfsptr@gmail.com>
2026-03-17 19:37:32 +0700

    feat: implementasi interaktif CLI dengan scanner dan loop menu

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,87 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Kopi kopi1 = new Kopi("Kopi Susu", 25000);
-        Kopi kopi2 = new Kopi("Espresso", 30000);
+        Scanner input = new Scanner(System.in);
 
-        Kue kue1 = new Kue("brownies", 35000);
+        Pelanggan pelanggan = new Pelanggan("luthfi", 50000);
 
-        Pelanggan pelanggan = new Pelanggan("luthfi", 100000);
+        boolean isRunning = true;
 
-        PelangganVIP pelangganVIP = new PelangganVIP("yoza", 50000);
-        pelangganVIP.beliItem(kopi2);
-        pelangganVIP.beliItem(kue1);
+        while (isRunning) {
+            System.out.println("\nHalo Selamat Datang di Cafe kami " + pelanggan.getNamaPelanggan());
+            System.out.println("Informasi Pelanggan:");
+            System.out.println("Nama    : " + pelanggan.getNamaPelanggan());
+            System.out.println("Saldo   : " + pelanggan.getSaldo());
+            System.out.println("");
+            System.out.println("1. Beli Kopi Susu.");
+            System.out.println("2. Beli Espresso.");
+            System.out.println("3. Beli Brownies");
+            System.out.println("4. Keluar");
 
-        System.out.println("Haloo! " + pelanggan.getNamaPelanggan() + ", Saldo Kamu: " + pelanggan.getSaldo());
-        System.out.println("1. Kopi: " + kopi1.getNama() + " | Harga: " + kopi1.getHarga());
-        System.out.println("2. Kopi: " + kopi2.getNama() + " | Harga: " + kopi2.getHarga());
-        System.out.println("3. Kue: " + kue1.getNama() + " | Harga: " + kue1.getHarga());
+            System.out.println("Pilih Menu");
+            int pilihan = input.nextInt();
+            input.nextLine();
 
-        pelanggan.beliItem(kopi1);
+            String konfirmasi;
+
+            switch (pilihan) {
+                case 1:
+                    Kopi kopi1 = new Kopi("Kopi Susu", 15000);
+
+                    do {
+                        System.out.println("Harga " + kopi1.getNama() + ": " + kopi1.getHarga());
+                        System.out.println("Ingin membeli ini? (y/n)");
+                        konfirmasi = input.next().toLowerCase();
+
+                    } while (!konfirmasi.equals("y") && !konfirmasi.equals("n"));
+
+                    if (konfirmasi.equals("y")) {
+                        pelanggan.beliItem(kopi1);
+                    } else {
+                        System.out.println("Pesanan dibatalkan");
+                    }
+                    break;
+                case 2:
+                    Kopi kopi2 = new Kopi("Espresso", 20000);
+
+                    do {
+                        System.out.println("Harga " + kopi2.getNama() + ": " + kopi2.getHarga());
+                        System.out.println("Ingin membeli ini? (y/n)");
+                        konfirmasi = input.next().toLowerCase();
+
+                    } while (!konfirmasi.equals("y") && !konfirmasi.equals("n"));
+
+                    if (konfirmasi.equals("y")) {
+                        pelanggan.beliItem(kopi2);
+                    } else {
+                        System.out.println("Pesanan dibatalkan");
+                    }
+                    break;
+                case 3:
+                    Kue kue = new Kue("Brownies", 10000);
+
+                    do {
+                        System.out.println("Harga " + kue.getNama() + ": " + kue.getHarga());
+                        System.out.println("Ingin membeli ini? (y/n)");
+                        konfirmasi = input.next().toLowerCase();
+
+                    } while (!konfirmasi.equals("y") && !konfirmasi.equals("n"));
+
+                    if (konfirmasi.equals("y")) {
+                        pelanggan.beliItem(kue);
+                    } else {
+                        System.out.println("Pesanan dibatalkan");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Selamat Menikmati");
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Pilihan Tidak Valid");
+            }
+        }
     }
 
 }
--- src/Pelanggan.java
+++ src/Pelanggan.java
@@ -30,7 +30,7 @@ public class Pelanggan {
     public void beliItem(ItemMenu item) {
         if (saldo >= item.getHarga()) {
             saldo -= item.getHarga();
-            System.out.println("Pembelian SUKSES: Selamat menikmati kopinya" + item.getNama());
+            System.out.println("Pembelian SUKSES: Selamat menikmati " + item.getNama());
         } else {
             System.out.println("Pembelian GAGAL: Maaf, saldo anda kurang");
         }
